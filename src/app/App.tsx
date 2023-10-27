2a9e5f31058237c6a528f08be5b66cdc328fc7a8
Olga Volobueva <olvik1605@gmail.com>
2023-10-27 16:59:24 -0400

    part6_Feature-Sliced Design

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,23 +3,23 @@ import './styles/index.scss';
 
 import {Link} from 'react-router-dom';
 
-import {AboutPageAsync} from './pages/AboutPage/AboutPage.async';
-import {MainPageAsync} from './pages/MainPage/MainPage.async';
-import {Suspense, useContext, useState} from 'react';
-import {Theme, ThemeContext} from './theme/ThemeContext';
-import { useTheme } from './theme/useTheme';
+import {Suspense} from 'react';
+import {classNames} from 'shared/lib/classNames/classNames';
+import {useTheme} from 'app/providers/ThemeProvider';
+import AboutPage from 'pages/AboutPage/ui/AboutPage';
+import MainPage from 'pages/MainPage/ui/MainPage';
 
 const App = () => {
 	const { theme, toggleTheme } = useTheme()
 	return (
-		<div className={`app ${theme}`}>
+		<div className={classNames('app', {}, [theme])}>
 			<button onClick={toggleTheme}>TOGGLE</button>
 			<Link to={'/'}>Главная</Link>
 			<Link to={'/about'}>О сайте</Link>
 			<Suspense fallback={<div>Loading...</div>}>
 				<Routes>
-					<Route path={'/about'} element={<AboutPageAsync />}/>
-					<Route path={'/'} element={<MainPageAsync />}/>
+					<Route path={'/about'} element={<AboutPage />}/>
+					<Route path={'/'} element={<MainPage />}/>
 				</Routes>
 			</Suspense>
 		</div>
