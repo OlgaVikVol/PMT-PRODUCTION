41b27d25cb8489365a71bf6a083bba800cf95fb3
Olga Volobueva <olvik1605@gmail.com>
2023-11-22 20:55:18 -0500

    part19_ErrorBoundary

---
+++ src/widgets/PageError/ui/PageError.1.tsx
@@ -0,0 +1,15 @@
+import { classNames } from 'shared/lib/classNames/classNames';
+import cls from './PageError.module.scss';
+import { useTranslation } from 'react-i18next';
+import { PageErrorProps } from './PageError';
+
+
+export const PageError = ({ className }: PageErrorProps) => {
+	const { t } = useTranslation();
+
+	return (
+		<div className={classNames(cls.PageError, {}, [className])}>
+			<p>{t("Произошла непредвиденная ошибка")}</p>
+		</div>
+	);
+};
