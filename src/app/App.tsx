9f1fdf6518dec495b0eb38a81a828eefd1c5ee68
Olga Volobueva <olvik1605@gmail.com>
2023-11-27 21:09:58 -0500

    part31_fix build

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,9 +2,9 @@ import './styles/index.scss';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { useTheme } from 'app/providers/ThemeProvider';
 import { Navbar } from 'widgets/Navbar';
-import { Sidebar } from 'widgets/Sidebar';
 import { Suspense } from 'react';
 import { AppRouter } from './providers/router';
+import { Sidebar } from 'widgets/Sidebar/ui/Sidebar/Sidebar';
 
 const App = () => {
     const { theme } = useTheme();
