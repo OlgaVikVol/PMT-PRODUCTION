9ca172a5b110a0ec9867bceb7ed9b5a62eef2556
Olga Volobueva <olvik1605@gmail.com>
2023-12-12 22:13:46 -0500

    part69_fix ts

--- src/shared/lib/components/DynamicModuleLoader/DynamicModuleLoader.tsx
+++ src/shared/lib/components/DynamicModuleLoader/DynamicModuleLoader.tsx
@@ -38,6 +38,7 @@ export const DynamicModuleLoader: FC<DynamicModuleLoaderProps> = (props) => {
                 });
             }
         };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
     return (
