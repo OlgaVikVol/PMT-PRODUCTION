d515b35f933184778a178bcb231f9b8d67519bb2
Olga Volobueva <olvik1605@gmail.com>
2023-12-14 00:05:03 -0500

    part74_add profile

---
+++ src/shared/const/common.ts
@@ -0,0 +1,13 @@
+export enum Currency {
+	'RUB' = 'RUB',
+	'EUR' = 'EUR',
+	'USD' = 'USD',
+}
+
+export enum Country {
+	Canada = 'Canada',
+	Belarus = 'Belarus',
+	Russia = 'Russia',
+	Kazakhstan = 'Kazahstan',
+	Armenia = 'Armenia',
+}
