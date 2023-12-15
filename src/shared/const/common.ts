a6afe855ac2e89eca58ddc13c3f893bc3805adf9
Olga Volobueva <olvik1605@gmail.com>
2023-12-14 19:03:20 -0500

    part76_fix build

--- src/shared/const/common.ts
+++ src/shared/const/common.ts
@@ -1,13 +1,13 @@
 export enum Currency {
-	'RUB' = 'RUB',
-	'EUR' = 'EUR',
-	'USD' = 'USD',
+  RUB = 'RUB',
+  EUR = 'EUR',
+  USD = 'USD',
 }
 
 export enum Country {
-	Canada = 'Canada',
-	Belarus = 'Belarus',
-	Russia = 'Russia',
-	Kazakhstan = 'Kazahstan',
-	Armenia = 'Armenia',
+  Canada = 'Canada',
+  Belarus = 'Belarus',
+  Russia = 'Russia',
+  Kazakhstan = 'Kazahstan',
+  Armenia = 'Armenia',
 }
