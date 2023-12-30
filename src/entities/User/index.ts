fc7f3a018a61829e653771f0461899151ac4abd0
Olga Volobueva <olvik1605@gmail.com>
2023-12-29 21:31:01 -0500

    part84_update app router

--- src/entities/User/index.ts
+++ src/entities/User/index.ts
@@ -3,6 +3,10 @@ export {
 } from './model/selectors/getUserAuthData/getUserAuthData';
 
 export {
+    getUserInited,
+} from './model/selectors/getUserInited/getUserInited';
+
+export {
     UserSchema,
     User,
 } from './model/types/user';
