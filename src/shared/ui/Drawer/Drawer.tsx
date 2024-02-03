f1a3bb4d0c28fbb718462a86d8f71308c1902fff
Olga Volobueva <olvik1605@gmail.com>
2024-02-03 17:59:01 -0500

    part127_add useModal hook, upgrade drawer and modal

--- src/shared/ui/Drawer/Drawer.tsx
+++ src/shared/ui/Drawer/Drawer.tsx
@@ -1,33 +1,52 @@
-import { Mods, classNames } from 'shared/lib/classNames/classNames';
-import { ReactNode, memo } from 'react';
+import { classNames, Mods } from 'shared/lib/classNames/classNames';
+import React, { memo, ReactNode } from 'react';
 import { useTheme } from 'app/providers/ThemeProvider';
-import { Portal } from '@headlessui/react';
-import cls from './Drawer.module.scss';
+import { useModal } from 'shared/lib/hooks/useModal/useModal';
 import { Overlay } from '../Overlay/Overlay';
+import cls from './Drawer.module.scss';
+import { Portal } from '../Portal/Portal';
 
 interface DrawerProps {
-  className?: string;
-  children: ReactNode;
-  isOpen?: boolean;
-  onClose?: () => void;
+    className?: string;
+    children: ReactNode;
+    isOpen?: boolean;
+    onClose?: () => void;
+    lazy?: boolean;
 }
 
 export const Drawer = memo((props: DrawerProps) => {
     const {
         className,
         children,
-        isOpen,
         onClose,
+        isOpen,
+        lazy,
     } = props;
     const { theme } = useTheme();
+
+    const {
+        close,
+        isClosing,
+        isMounted,
+    } = useModal({
+        animationDelay: 300,
+        onClose,
+        isOpen,
+    });
+
     const mods: Mods = {
         [cls.opened]: isOpen,
+        [cls.isClosing]: isClosing,
     };
+
+    if (lazy && !isMounted) {
+        return null;
+    }
 
     return (
         <Portal>
-            <div className={classNames(cls.Drawer, mods, [className])}>
-                <Overlay onClick={onClose} />
+            <div className={classNames(cls.Drawer, mods, [className, theme, 'app_drawer'])}>
+                <Overlay onClick={close} />
                 <div
                     className={cls.content}
                 >
@@ -35,6 +54,5 @@ export const Drawer = memo((props: DrawerProps) => {
                 </div>
             </div>
         </Portal>
-
     );
 });
