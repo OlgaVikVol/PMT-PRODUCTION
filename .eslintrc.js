88fa02cfcfbd9038d462fd74994641d14531c9e4
Olga Volobueva <olvik1605@gmail.com>
2024-02-06 20:52:29 -0500

    part135_update custom eslint-plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,7 +22,7 @@ module.exports = {
         '@typescript-eslint',
         'i18next',
         'react-hooks',
-        'ulbi-tv-plugin',
+        'olga-plugin',
     ],
     rules: {
         'react/jsx-indent': [2, 4],
@@ -61,7 +61,7 @@ module.exports = {
         'no-undef': 'off',
         'react/no-array-index-key': 'off',
         'arrow-body-style': 'off',
-        'ulbi-tv-plugin/path-checker': 'error',
+        'olga-plugin/path-checker': ['error', { alias: '@' }],
     },
     globals: {
         __IS_DEV__: true,
