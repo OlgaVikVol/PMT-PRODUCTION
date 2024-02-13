1ba8cb82b4664af3ba49373cca662cd60e11d6d9
Olga Volobueva <olvik1605@gmail.com>
2024-02-13 14:44:39 -0500

    part150_add generic component

--- src/shared/ui/Select/Select.tsx
+++ src/shared/ui/Select/Select.tsx
@@ -1,5 +1,5 @@
 import { ChangeEvent, useMemo } from 'react';
-import { Mods, classNames } from '@/shared/lib/classNames/classNames';
+import { classNames, Mods } from '@/shared/lib/classNames/classNames';
 import cls from './Select.module.scss';
 
 export interface SelectOption<T extends string> {
@@ -8,40 +8,39 @@ export interface SelectOption<T extends string> {
 }
 
 interface SelectProps<T extends string> {
-  className?: string;
-  label?: string;
-  options?: SelectOption<T>[];
-  value?: T;
-  onChange?: (value: T) => void;
-  readonly?: boolean;
+    className?: string;
+    label?: string;
+    options?: SelectOption<T>[];
+    value?: T;
+    onChange?: (value: T) => void;
+    readonly?: boolean;
 }
 
-export const Select = <T extends string> (props: SelectProps<T>) => {
+export const Select = <T extends string>(props: SelectProps<T>) => {
     const {
         className,
         label,
         options,
-        value,
         onChange,
+        value,
         readonly,
     } = props;
 
     const onChangeHandler = (e: ChangeEvent<HTMLSelectElement>) => {
-        onChange?.(e.target.value as T);
+        if (onChange) {
+            onChange(e.target.value as T);
+        }
     };
 
-    // eslint-disable-next-line arrow-body-style
-    const optionsList = useMemo(() => {
-        return options?.map((opt) => (
-            <option
-                className={cls.option}
-                value={opt.value}
-                key={opt.value}
-            >
-                {opt.content}
-            </option>
-        ));
-    }, [options]);
+    const optionsList = useMemo(() => options?.map((opt) => (
+        <option
+            className={cls.option}
+            value={opt.value}
+            key={opt.value}
+        >
+            {opt.content}
+        </option>
+    )), [options]);
 
     const mods: Mods = {};
 
