c93a3aa8f8b0bdccc756b254da8bb251e4c64379
Olga Volobueva <olvik1605@gmail.com>
2024-03-22 20:53:30 -0400

    part204_fix build

--- src/widgets/ScrollToolbar/ui/ScrollToolbar/ScrollToolbar.tsx
+++ src/widgets/ScrollToolbar/ui/ScrollToolbar/ScrollToolbar.tsx
@@ -2,7 +2,7 @@ import { memo } from 'react';
 import { classNames } from '@/shared/lib/classNames/classNames';
 import { VStack } from '@/shared/ui/redesigned/Stack';
 import cls from './ScrollToolbar.module.scss';
-import { ScrollToTopButton } from '@/features/scrollToTopButton';
+import { ScrollToTopButton } from '@/features/scrollToTopButton/ui/ScrollToTopButton/ScrollToTopButton';
 
 interface ScrollToolbarProps {
   className?: string;
