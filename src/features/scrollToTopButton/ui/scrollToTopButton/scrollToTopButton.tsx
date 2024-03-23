ed1d4cefd4cacd371ff711b6f94cfc7796a24926
Olga Volobueva <olvik1605@gmail.com>
2024-03-22 20:46:14 -0400

    part203_fix build

--- src/features/scrollToTopButton/ui/scrollToTopButton/scrollToTopButton.tsx
+++ src/features/scrollToTopButton/ui/scrollToTopButton/scrollToTopButton.tsx
@@ -1,17 +1,17 @@
 import { memo } from 'react';
+import { classNames } from '@/shared/lib/classNames/classNames';
+import cls from './ScrollToTopButton.module.scss';
 import { Icon } from '@/shared/ui/redesigned/Icon';
 import CircleIcon from '@/shared/assets/icons/circle-up.svg';
-import cls from './ScrollToTopButton.module.scss';
-import { classNames } from '@/shared/lib/classNames/classNames';
 
 interface ScrollToTopButtonProps {
-  className?: string;
+    className?: string;
 }
 
 export const ScrollToTopButton = memo((props: ScrollToTopButtonProps) => {
     const { className } = props;
 
-    const onClick = () => {
+    const onCLick = () => {
         window.scrollTo({ top: 0, behavior: 'smooth' });
     };
 
@@ -19,7 +19,7 @@ export const ScrollToTopButton = memo((props: ScrollToTopButtonProps) => {
         <Icon
             Svg={CircleIcon}
             clickable
-            onClick={onClick}
+            onClick={onCLick}
             width={32}
             height={32}
             className={classNames(cls.ScrollToTopButton, {}, [className])}
